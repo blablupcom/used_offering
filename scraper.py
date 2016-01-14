42baa7624f004e635433dcd3314b6f18a27f1efb
blablupcom <o.volodin@yahoo.com>
2016-01-14 23:15:56 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import re
 
 
-start_url = 'http://www.amazon.com/gp/offer-listing/{}/ref%3Dolp_f_used?ie=UTF8&f_new=true&f_primeEligible=true'
+start_url = 'http://www.amazon.com/gp/offer-listing/{}/ref%3Dolp_f_used?ie=UTF8&f_primeEligible=true&f_used=true&f_usedAcceptable=true&f_usedGood=true&f_usedLikeNew=true&f_usedVeryGood=true'
 ua = {'User-agent': 'Mozilla/5.0'}
 
 
@@ -36,77 +36,166 @@ def parse(search_term, search_tag, p):
     search_tag = '='+'"'+search_tag+'"'
     print p
     soup = connect(start_url, search_term)
-    if 'new=true' in start_url:
-        search_rows = []
-        try:
-            search_rows = soup.find_all('div', 'a-row a-spacing-mini olpOffer')
-        except:
-            pass
-        if not search_rows:
-            new_price_absent = 1
-        else:
-            new_price_absent = 0
-        searchstring = search_term.strip()
-        search_tag = search_tag
-        new_price_lowest = new_price_0 = new_price_1 = new_price_2 = new_price_3 = new_price_4 = new_price_5 = new_price_6 = new_price_7 = new_price_8 = new_price_9 = new_price_amazon = ''
-        if new_price_absent == 0:
-            for i, search_row in enumerate(search_rows):
-                try:
-                    back_ordered = search_row.find('div', 'a-column a-span3 olpDeliveryColumn').find('ul', 'a-vertical').find('span', 'a-list-item').text.strip()
-                    if i == 0:
-                        new_price_lowest = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        new_price_0 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_0 = ''
-                    elif i == 1:
-                        new_price_1 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_1 = ''
-                    elif i == 2:
-                        new_price_2 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_2 = ''
-                    elif i == 3:
-                        new_price_3 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_3 = ''
-                    if i == 4:
-                        new_price_4 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_4 = ''
-                    elif i == 5:
-                        new_price_5 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_5 = ''
-                    if i == 6:
-                        new_price_6 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_6 = ''
-                    elif i == 7:
-                        new_price_7 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_7 = ''
-                    if i == 8:
-                        new_price_8 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_8 = ''
-                    elif i == 9:
-                        new_price_9 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                        if 'Back-ordered' in back_ordered:
-                            new_price_9 = ''
-                    amazon_label = search_row.find('h3', 'a-spacing-none olpSellerName').find('img')['alt']
-                    if 'Amazon.com' in amazon_label:
-                        new_price_amazon = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
-                except:
-                    pass
+    if 'used=true' in start_url:
+            search_rows = []
+            try:
+                search_rows = soup.find_all('div', 'a-row a-spacing-mini olpOffer')
+            except:
+                pass
+            if not search_rows:
+                used_price_absent = 1
+            else:
+                used_price_absent = 0
+            searchstring = search_term.strip()
+            search_tag = search_tag
+            used_price_lowest = used_price_0 = used_price_1 = used_price_2 = used_price_3 = used_price_4 = used_price_5 = used_price_6 = used_price_7 = used_price_8 = used_price_9 = used_cond_0 = used_cond_1 = used_cond_2 =\
+                        used_cond_3 = used_cond_4 = used_cond_5 = used_cond_6 = used_cond_7 = used_cond_8 =  used_cond_9 = ''
+            if used_price_absent == 0:
+                for i, search_row in enumerate(search_rows):
+                    try:
+                        back_ordered = search_row.find('div', 'a-column a-span3 olpDeliveryColumn').find('ul', 'a-vertical').find('span', 'a-list-item').text.strip()
+                        if i == 0:
+                            used_price_lowest = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            used_price_0 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_0 = 4
+                            elif '- Good' in status_check:
+                                used_cond_0 = 3
+                            elif '- Very Good' in status_check:
+                                used_cond_0 = 2
+                            elif '- Like New' in status_check:
+                                used_cond_0 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_0 = used_price_0 = ''
+                        elif i == 1:
+                            used_price_1 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_1 = 4
+                            if '- Good' in status_check:
+                               used_cond_1 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_1 = 2
+                            if '- Like New' in status_check:
+                                used_cond_1 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_1 = used_price_0 = ''
+                        elif i == 2:
+                            used_price_2 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_2 = 4
+                            if '- Good' in status_check:
+                                used_cond_2 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_2 = 2
+                            if '- Like New' in status_check:
+                                used_cond_2 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_2 = used_price_2 = ''
+                        elif i == 3:
+                            used_price_3 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_3 = 4
+                            if '- Good' in status_check:
+                                used_cond_3 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_3 = 2
+                            if '- Like New' in status_check:
+                                used_cond_3 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_3 = used_price_3 = ''
+                        if i == 4:
+                            used_price_4 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_4 = 4
+                            if '- Good' in status_check:
+                                used_cond_4 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_4 = 2
+                            if '- Like New' in status_check:
+                                used_cond_4 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_4 = used_price_4 = ''
+                        elif i == 5:
+                            used_price_5 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_5 = 4
+                            if '- Good' in status_check:
+                                used_cond_5 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_5 = 2
+                            if '- Like New' in status_check:
+                                used_cond_5 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_5 = used_price_5 = ''
+                        if i == 6:
+                            used_price_6 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_6 = 4
+                            if '- Good' in status_check:
+                                used_cond_6 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_6 = 2
+                            if '- Like New' in status_check:
+                                used_cond_6 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_6 = used_price_6 = ''
+                        elif i == 7:
+                            used_price_7 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_7 = 4
+                            if '- Good' in status_check:
+                                used_cond_7 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_7 = 2
+                            if '- Like New' in status_check:
+                                used_cond_7 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_7 = used_price_7 = ''
+                        if i == 8:
+                            used_price_8 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_8 = 4
+                            if '- Good' in status_check:
+                               used_cond_8 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_8 = 2
+                            if '- Like New' in status_check:
+                                used_cond_8 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_8 = used_price_8 = ''
+                        elif i == 9:
+                            used_price_9 = search_row.find('span', 'a-size-large a-color-price olpOfferPrice a-text-bold').text.strip().split('$')[1].strip()
+                            status_check = search_row.find('span', 'a-size-medium olpCondition a-text-bold').text.strip()
+                            if '- Acceptable' in status_check:
+                                used_cond_9 = 4
+                            if '- Good' in status_check:
+                                used_cond_9 = 3
+                            if '- Very Good' in status_check:
+                                used_cond_9 = 2
+                            if '- Like New' in status_check:
+                                used_cond_9 = 1
+                            if 'Back-ordered' in back_ordered:
+                                used_cond_9 = used_price_9 = ''
 
-        else:
-            new_price_lowest = new_price_0 = new_price_1 = new_price_2 = new_price_3 = new_price_4 = new_price_5 = new_price_6 = new_price_7 = new_price_8 = new_price_9 = new_price_amazon = ''
-        today_date = str(datetime.now())
-        scraperwiki.sqlite.save(unique_keys=['Date'], data={'SearchString': unicode(searchstring), 'Search Tag': search_tag, 'new_price_lowest': new_price_lowest, 'new_price_0': new_price_0, 'new_price_1': new_price_1, 'new_price_2': new_price_2,
-                                                                'new_price_3': new_price_3, 'new_price_4': new_price_4, 'new_price_5': new_price_5, 'new_price_6': new_price_6, 'new_price_7': new_price_7, 'new_price_8': new_price_8, 'new_price_9': new_price_9,
-                                                                'new_price_amazon': new_price_amazon, 'new_price_absent': new_price_absent, 'Date': today_date})
+                    except:pass
+            else:
+                used_price_lowest = used_price_0 = used_price_1 = used_price_2 = used_price_3 = used_price_4 = used_price_5 = used_price_6 = used_price_7 = used_price_8 = used_price_9 = used_cond_0 = used_cond_1 = used_cond_2 =\
+                        used_cond_3 = used_cond_4 = used_cond_5 = used_cond_6 = used_cond_7 = used_cond_8 =  used_cond_9 = ''
 
+            today_date = str(datetime.now())
+            scraperwiki.sqlite.save(unique_keys=['Date'], data={'SearchString': unicode(searchstring), 'Search Tag': search_tag, 'used_price_lowest': used_price_lowest,'used_price_0': used_price_0, 'used_price_1': used_price_1, 'used_price_2': used_price_2,
+                                                                    'used_price_3': used_price_3, 'used_price_4': used_price_4, 'used_price_5': used_price_5, 'used_price_6': used_price_6, 'used_price_7': used_price_7, 'used_price_8': used_price_8
+                    , 'used_price_9': used_price_9, 'used_cond_0': used_cond_0, 'used_cond_1': used_cond_1, 'used_cond_2': used_cond_2, 'used_cond_3': used_cond_3, 'used_cond_4': used_cond_4, 'used_cond_5': used_cond_5, 'used_cond_6': used_cond_6,
+                      'used_cond_7': used_cond_7, 'used_cond_8': used_cond_8, 'used_cond_9': used_cond_9, 'used_price_absent': used_price_absent, 'Date': today_date})
 
 if __name__ == '__main__':
         file1 = open('amazon.txt', 'r')
